a8ff3292f791cefb4d689d9df2e7c8d2a5116559
Michał Listek <michal.listex@gmail.com>
2022-06-23 14:24:22 +0200

    created slider on main page

--- force-app/main/default/lwc/bestBooksComponent/bestBooksComponent.js
+++ force-app/main/default/lwc/bestBooksComponent/bestBooksComponent.js
@@ -1,11 +1,60 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import getTopBooks from '@salesforce/apex/BookListController.getTopBooks';
 
 export default class BookListComponent extends NavigationMixin(LightningElement) {
 
-  @wire(getTopBooks)
-  books;
+  books = [];
+  error;
+  currentBooks = [];
+  currentStartIndex = 0;
+
+  connectedCallback() {
+    this.getBooks();
+  }
+
+  getBooks() {
+    getTopBooks()
+    .then((result) => {
+      this.books = result;
+      this.currentBooks = this.books.slice(this.currentStartIndex, this.currentStartIndex + 3);
+      
+      setInterval(() => {
+        this.handleForward();
+      }, 4000);
+
+    }).catch((err) => {
+      this.error = err.body.message;
+    });
+  }
+
+  handleBack() {
+    this.currentBooks = [];
+    this.currentStartIndex--;
+
+    if (this.currentStartIndex < 0) {
+      this.currentStartIndex = this.books.length - 1;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      let index = (this.currentStartIndex + i) % this.books.length;
+      this.currentBooks.push(this.books[index]);
+    }
+  }
+
+  handleForward() {
+    this.currentBooks = [];
+    this.currentStartIndex++;
+
+    if (this.currentStartIndex > this.books.length - 1) {
+      this.currentStartIndex = 0;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      let index = (this.currentStartIndex + i) % this.books.length;
+      this.currentBooks.push(this.books[index]);
+    }
+  }
 
   handleBookView(event) {
     const bookId = event.detail;
